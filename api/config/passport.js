7ba2b7bf7f290622b5937732313119764c1095b9
pedrohenriquecardoso <pedrohcardoso.1995@gmail.com>
2021-11-16 21:01:02 -0300

    passportjs configs

---
+++ api/config/passport.js
@@ -0,0 +1,31 @@
+const passport = require('passport')
+const passportJwt = require('passport-jwt')
+const {Strategy, ExtractJwt} = passportJwt
+require('dotenv').config()
+
+module.exports = app => {
+    const params = {
+        secretOrKey: process.env.DB_AUTH,
+        jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+    }
+    const strategy = new Strategy(params, (payload, done) => {
+        app.db('users')
+            .where({id: payload.id})
+            .first()
+            .then(user => {
+                if (user) {
+                    done(null, {id: user.id, email: user.email})
+                } else {
+                    done(null, false)
+                }
+            })
+            .catch(err => done(err, false))
+    })
+
+    passport.use(strategy)
+
+    return {
+        initialize: () => passport.inicialize(),
+        authenticate: () => passport.authenticate('jwt', {session: false})
+    }
+}
