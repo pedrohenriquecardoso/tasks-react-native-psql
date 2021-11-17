25d8eca80780901ddcb92c45e913e9f3ef827f24
pedrohenriquecardoso <pedrohcardoso.1995@gmail.com>
2021-11-16 21:24:04 -0300

    get, save, remove tasks

---
+++ api/api/task.js
@@ -0,0 +1,41 @@
+const moment = require('moment')
+
+module.exports = app => {
+    const getTasks = (req, res) => {
+        const date = req.query.date ? req.query.date
+            : moment().endOf('day').toDate()
+        app.db('tasks')
+            .where({userId: req.user.id})
+            .where('estimateAt', '<=', date)
+            .orderBy('estimateAt')
+            .then(tasks => res.json(tasks))
+            .catch(err => res.status(500).json(err))
+    }
+
+    const save = (req, res) => {
+        if (!req.body.desc.trim()){
+            return res.status(400).send('inform the description')
+        }
+        req.body.userId = req.user.id
+
+        app.db('tasks')
+            .indert(req.body)
+            .then(_ => res.status(204).send())
+            .catch(err => res.status(400).json(err))
+    }
+
+    const remove = (req, res) => [
+        app.db('tasks')
+            .where({id: req.params.id, userId: req.user.id})
+            .del()
+            .then(rowsDeleted => {
+                if (rowsDeleted > 0){
+                    res.status(204).send()
+                } else {
+                    const msg = `Task id ${req.params.id} are not found` 
+                    res.status(400).send(msg)
+                }
+            })
+            .catch(err => res.status(500).json(err))
+    ]
+}
